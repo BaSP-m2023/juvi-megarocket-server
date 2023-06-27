bc001113459b71e4a8674bb3ae2d5affcd117b2e
octavi01 <octaferreyraleon@gmail.com>
2023-06-27 18:13:16 -0300

    MR-174: delete middlewares of the routeś servers for the QA testing

--- src/routes/member.js
+++ src/routes/member.js
@@ -1,15 +1,14 @@
 import { Router } from 'express';
 import memberController from '../controllers/member';
 import validationsMember from '../validations/member';
-import verifyToken from '../middlewares/authMiddleware';
 
 const memberRouter = Router();
-memberRouter.get('/', verifyToken(['ADMIN']), memberController.getMembers);
-memberRouter.post('/', verifyToken(['ADMIN']), validationsMember.validateCreate, memberController.createMember);
+memberRouter.get('/', memberController.getMembers);
+memberRouter.post('/', validationsMember.validateCreate, memberController.createMember);
 
-memberRouter.get('/:id', verifyToken(['ADMIN', 'MEMBER']), memberController.getMemberById);
-memberRouter.get('/filter/:nameMember', verifyToken(['ADMIN', 'MEMBER']), memberController.filterMember);
-memberRouter.put('/:id', verifyToken(['ADMIN', 'MEMBER']), validationsMember.validateUpdate, memberController.updateMember);
-memberRouter.delete('/:id', verifyToken(['ADMIN']), memberController.deleteMember);
+memberRouter.get('/:id', memberController.getMemberById);
+memberRouter.get('/filter/:nameMember', memberController.filterMember);
+memberRouter.put('/:id', validationsMember.validateUpdate, memberController.updateMember);
+memberRouter.delete('/:id', memberController.deleteMember);
 
 export default memberRouter;
